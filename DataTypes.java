d05d20b333f051286e6293421950f61d87441815
Manana-19 <Mananjerath9582@gmail.com>
2024-07-28 14:37:28 +0530

    DataTypes

---
+++ DataTypes.java
@@ -0,0 +1,50 @@
+public class DataTypes {
+    public static void main(String[] Args) {
+        // Listing basics of datatypes
+
+        // Integer
+        int x = 10, y = 20;
+
+        //Float
+        float a = 1.0f; // f at the end of the number is important
+
+        //double
+        double b = 3.0d; // similar to float, d at the end is important for syntax
+
+        // char
+        char c = 'C'; // For a single character, char is used.... IMPORTANT: USE SINGLE INVERTED COMMA FOR THE CHARACTER DATA TYPE
+
+        // String
+        String str = "Lmao"; //For groups of char which we call string, we have to use Double inverted commas also known as quotes
+
+        /*
+         * These were the basics, time to jump into something really informative
+         * 
+         * Data Types are of 2 types.
+         * 
+         * Primative -> The absolute datatypes that is available in Java language from it's creation.
+         * 
+         * Non-Primaiive -> Datatypes derived from Primitive Datatypes (like string)
+         */
+
+        // Numerical info for each primitive types (amount of memory they take)
+
+        // byte (1 byte) -> [-128, 127]
+
+        // short (2 byte) -> [-32,768, 32767]
+
+        // int (4 byte) -> [- 2B, 2B ]
+
+        // long (8 byte) -> [ -9 Quintrillion (Billion -> Trillion -> Quadrillion -> Quinttrillion) , 9 Quintrillion]
+
+        // float (4 bytes) -> good for 7-8 decimal digits
+
+        // double (8 bytes) -> good for 15 decimal digits
+
+        // boolean (1 bit) -> true or false (0 or 1)
+
+        // char (2 bytes) -> Stores 1 ASCII character value
+
+        System.out.println("Datatypes description in the code comments");
+    }
+}
