b69d859c6ca3793904f69db6dc332b899cedfcd1
Manana-19 <Mananjerath9582@gmail.com>
2024-08-09 09:44:38 +0530

    Objects and Array of Objects

--- Objects.java
+++ Objects.java
@@ -3,18 +3,44 @@ class Person {
     byte age;
     String name;
 
-    public void Set(String Name, byte Age) {
-        age = Age; name = Name;
+    Person(String n, byte a) {
+        name=n;
+        age=a;
+    }
+
+    byte Set(String Name, byte Age) {
+        age = Age; 
+        name = Name;
+        return 0;
     }
     
     String info() {
-        return "Name =>"+name+"\nAge => "+age;
+        return "Name => "+name+"\nAge => "+age;
     };
 };
 
 
 public class Objects {
 
-    Person Some = new Person();
+    public static void main(String[] Args) {
 
-}
+        // Creating the list of objects
+
+        Person[] obj = new Person[3];
+
+        // Creating for loop
+        for (byte i = 0; i < 3; i++) {
+            
+            // Initializing the object 
+            obj[i] = new Person("Something", (byte) (i+12));
+
+            // Creating the reference
+            Person Something =  obj[i];
+
+            System.out.println(Something.info());
+            
+        }
+
+    }
+
+};
---
+++ Strings.java
@@ -0,0 +1,3 @@
+public class Strings {
+// Will work on it soon    
+}
