85316d0567894663bf4cb49948c3b0f74d756a86
Manana-19 <Mananjerath9582@gmail.com>
2024-08-11 23:41:39 +0530

    Arrays (1/2)

---
+++ Arrays.java
@@ -0,0 +1,17 @@
+public class Arrays {
+    public static void main(String[] Args) {
+
+        // In order to declare an Array, we have to put [] square brackets in front of type variables
+
+        int[] nums = {1,2,3,4,5};
+
+        // We can also Initialize the blank array with given length but it will be blank and will not go out of bounds;
+
+        int[] arr = new int[5];
+
+        System.out.println(arr[0]); // Output: 0
+
+        // We can also create objects or String arrays as demonstrated in Objects.java
+
+    }
+}
